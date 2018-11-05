16d9139af5f3bdf0a946469b33b78443ebf13631
sangamesh somawar <sangameshsomawar@Apples-MacBook-Pro-2.local>
2018-11-05 20:24:54 +0530

    add sitebar route

--- app/Routes.js
+++ app/Routes.js
@@ -4,12 +4,22 @@ import routes from './constants/routes';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
 import CounterPage from './containers/CounterPage';
+import Header from './Header';
+import SideBar from './SideBar';
 
 export default () => (
   <App>
-    <Switch>
-      <Route path={routes.COUNTER} component={CounterPage} />
-      <Route path={routes.HOME} component={HomePage} />
-    </Switch>
+    <Header />
+    <div className="row">
+      <div className="col-sm-2">
+        <SideBar />
+      </div>
+      <div className="col-sm-10">
+        <Switch>
+          <Route path={routes.COUNTER} component={CounterPage} />
+          <Route path={routes.HOME} component={HomePage} />
+        </Switch>
+      </div>
+    </div>
   </App>
 );
